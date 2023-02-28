68954c0e1e286b1ba23ff5bddc509f0afb56fcd4
staceysequeira <ssequ002@gmail.com>
2023-02-28 01:23:38 +0000

    My first commit

--- src/js/app.js
+++ src/js/app.js
@@ -8,7 +8,7 @@ import "../style/index.css";
         includeCover: true, // if includeCover is true the algorithm should
         background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
         avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "left", // social media bar position (left or right)
+        socialMediaPosition: "position-left", // social media bar position (position-left or position-right)
         
         twitter: null, // social media usernames
         github: null,
@@ -29,21 +29,42 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  let twitter = "";
+  if (variables.twitter) {
+    twitter = `<li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>`;
+  }
+
+  let github = "";
+  if (variables.github) {
+    github = `<li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>`;
+  }
+
+  let linkedin = "";
+  if (variables.linkedin) {
+    linkedin = `<li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>`;
+  }
+
+  let instagram = "";
+  if (variables.instagram) {
+    instagram = `<li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>`;
+  }
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name || " "} 
+          ${variables.lastname || " "} </h1>
+          <h2>${variables.role || " "}</h2>
+          <h3>${variables.city || " "} ${variables.country || " "}</h3>
+          <ul class=${variables.socialMediaPosition}>
+            ${twitter}
+            ${github}
+            ${linkedin}
+            ${instagram}
           </ul>
         </div>
-    `;
+`;
 }
 
 /**
@@ -61,7 +82,7 @@ window.onload = function() {
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: null,
     linkedin: null,
     instagram: null,
     name: null,
